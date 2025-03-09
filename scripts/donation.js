1b3c4a38150b8882993966fe0dd0779d6ed37d22
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-03-09 19:00:56 +0600

    updated UI

--- scripts/donation.js
+++ scripts/donation.js
@@ -1,156 +1,32 @@
-document.getElementById("donate-btn-1").addEventListener("click", function () {
-  const donation = inputValueGetById("donation-input-1");
-  if (isNaN(donation) || donation <= 0) {
-    alert("Invalid Donation Amount");
-    return;
-  } else {
-    my_modal_1.showModal();
-  }
+const totalBudget = document.getElementById("total-budget");
 
-  const totalcollection = numberGetByText("total-collection");
-  document.getElementById("total-collection").innerText =
-    totalcollection + donation;
-
-  const availableBalance = numberGetByText("total-budget");
-
-  const remainingBalance = availableBalance - donation;
-
-  if (remainingBalance < 0) {
-    window.location.reload();
-  }
-
-  setElementById("total-budget", remainingBalance);
-
-  const donationSection = document.getElementById("donation-section");
-  const div = document.createElement("div");
-  div.classList = "bg-white rounded-md text-black p-3 border-2 border-gray-500";
-
-  const date = new Date();
-
-  const day = date.toLocaleDateString("en-US", { weekday: "short" });
-
-  const month = date.toLocaleDateString("en-US", { month: "short" });
-
-  const dayOfMonth = date.getDate();
-
-  const currentTime = new Date().toLocaleTimeString();
-
-  const newYear = new Date().getFullYear();
-  const p = document.createElement("p");
-  p.innerHTML = `
-  <p>${donation} Taka is Donated for Flood at Noakhali,Bangladesh</p>
-  <p class="bg-gray-800 p-2 bg-opacity-10">Date: ${day}, ${month} ${dayOfMonth}, ${newYear} ${currentTime}</p>
-  `;
-
-  div.appendChild(p);
-  donationSection.appendChild(div);
-  document.getElementById("donation-input-1").value = "";
+const donateAllBtn = document.querySelectorAll(".donate-btn");
+donateAllBtn.forEach((donateBtn) => {
+  donateBtn.addEventListener("click", () => {
+    handleDonate(donateBtn);
+  });
 });
 
-document.getElementById("donate-btn-2").addEventListener("click", function () {
-  const donation = inputValueGetById("donation-input-2");
-  if (isNaN(donation) || donation <= 0) {
-    alert("Invalid Donation Amount");
+const updateBudget = (inputValue) => {
+  const budget = parseFloat(totalBudget.textContent);
+
+  setElementById("total-budget", budget + inputValue);
+};
+
+const handleDonate = (btn) => {
+  const card = btn.closest(".card");
+  const donateAmountValue = parseFloat(card.querySelector(".input").value);
+  if (isNaN(donateAmountValue) || donateAmountValue < 0) {
+    alert("Should be Valid");
     return;
   }
+  updateBudget(donateAmountValue);
+  const totalCollection = parseFloat(
+    card.querySelector(".total-collection").textContent
+  );
+  card.querySelector(".total-collection").textContent = (
+    totalCollection + donateAmountValue
+  ).toFixed(2);
 
-  const totalcollection2 = numberGetByText("total-collection-feni");
-  document.getElementById("total-collection-feni").innerText =
-    totalcollection2 + donation;
-
-  const availableBalance = numberGetByText("total-budget");
-  const remainingBalance = availableBalance - donation;
-
-  if (remainingBalance < 0) {
-    window.location.reload();
-  }
-  setElementById("total-budget", remainingBalance);
-
-  const donationSection = document.getElementById("donation-section");
-  const div = document.createElement("div");
-  div.classList = "bg-white rounded-md text-black p-3 border-2 border-gray-500";
-
-  const date = new Date();
-
-  const day = date.toLocaleDateString("en-US", { weekday: "short" });
-
-  const month = date.toLocaleDateString("en-US", { month: "short" });
-
-  const dayOfMonth = date.getDate();
-
-  const currentTime = new Date().toLocaleTimeString();
-
-  const newYear = new Date().getFullYear();
-  const p = document.createElement("p");
-  p.innerHTML = `
-  <p>${donation} Taka is Donated for Flood at Feni,Bangladesh</p>
-  <p class="bg-gray-800 p-2 bg-opacity-10">Date: ${day}, ${month} ${dayOfMonth}, ${newYear} ${currentTime}</p>
-  `;
-
-  div.appendChild(p);
-  donationSection.appendChild(div);
-  document.getElementById("donation-input-2").value = "";
-});
-
-document.getElementById("donate-btn-3").addEventListener("click", function () {
-  const donation = inputValueGetById("donation-input-3");
-  if (isNaN(donation) || donation <= 0) {
-    alert("Invalid Donation Amount");
-    return;
-  }
-
-  const totalcollection3 = numberGetByText("total-collection-quota");
-  document.getElementById("total-collection-quota").innerText =
-    totalcollection3 + donation;
-
-  const availableBalance = numberGetByText("total-budget");
-  const remainingBalance = availableBalance - donation;
-  if (remainingBalance < 0) {
-    window.location.reload();
-  }
-  setElementById("total-budget", remainingBalance);
-
-  const donationSection = document.getElementById("donation-section");
-  const div = document.createElement("div");
-  div.classList = "bg-white rounded-md text-black p-3 border-2 border-gray-500";
-
-  const date = new Date();
-
-  const day = date.toLocaleDateString("en-US", { weekday: "short" });
-
-  const month = date.toLocaleDateString("en-US", { month: "short" });
-
-  const dayOfMonth = date.getDate();
-
-  const currentTime = new Date().toLocaleTimeString();
-
-  const newYear = new Date().getFullYear();
-  const p = document.createElement("p");
-  p.innerHTML = `
-  <p>${donation} Taka is Donated for the Injures in the Quota Movement</p>
-  <p class="bg-gray-800 p-2 bg-opacity-10">Date: ${day}, ${month} ${dayOfMonth}, ${newYear} ${currentTime}</p>
-  `;
-
-  div.appendChild(p);
-  donationSection.appendChild(div);
-  document.getElementById("donation-input-3").value = "";
-});
-
-const history = document.getElementById("history");
-const donation = document.getElementById("donation");
-
-document.getElementById("history").addEventListener("click", function () {
-  hideElementById("card-section");
-  showElementById("history-section");
-  donation.classList.remove("bg-btn-color");
-  history.classList.add("bg-btn-color");
-  history.classList.remove("bg-gray-200");
-});
-
-document.getElementById("donation").addEventListener("click", function () {
-  showElementById("card-section");
-  hideElementById("history-section");
-
-  donation.classList.add("bg-btn-color");
-  history.classList.remove("bg-btn-color");
-});
+  card.querySelector(".input").value = "";
+};
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,28 +1,5 @@
+document.getElementsByTagName('html')[0].setAttribute('data-theme', 'light');
 
-
-const html = document.getElementsByTagName("html")[0];
-html.setAttribute("data-theme", "light");
-
-document.getElementById("blog").addEventListener("click", function () {
-  window.location.href = "./blog.html";
-});
-
-function inputValueGetById(elementId) {
-  return Number(document.getElementById(elementId).value);
-}
-
-function numberGetByText(elementId) {
-  return Number(document.getElementById(elementId).innerText);
-}
-
-function setElementById(elementId, value) {
-  document.getElementById(elementId).innerText = value;
-}
-
-function hideElementById(elementId) {
-  document.getElementById(elementId).classList.add("hidden");
-}
-
-function showElementById(elementId) {
-  document.getElementById(elementId).classList.remove("hidden");
+const setElementById = (id, valueInput) => {
+  document.getElementById(id).textContent = valueInput.toFixed(2);
 }
