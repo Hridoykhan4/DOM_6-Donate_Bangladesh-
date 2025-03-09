4d48128ff52a736060f31e44913152d05fefeb7f
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-03-09 23:24:45 +0600

    final push

--- scripts/donation.js
+++ scripts/donation.js
@@ -10,16 +10,62 @@ donateAllBtn.forEach((donateBtn) => {
 const updateBudget = (inputValue) => {
   const budget = parseFloat(totalBudget.textContent);
 
-  setElementById("total-budget", budget + inputValue);
+  setElementById("total-budget", budget - inputValue);
+};
+
+document.getElementById("history-btn").addEventListener("click", () => {
+  document
+    .getElementById("donation-btn")
+    .classList.remove("bg-btn-color", "hover:bg-btn-color");
+  document
+    .getElementById("history-btn")
+    .classList.add("bg-btn-color", "hover:bg-btn-color");
+  hideElementById("card-section");
+  showElementById("history-section");
+});
+document.getElementById("donation-btn").addEventListener("click", () => {
+  document
+    .getElementById("history-btn")
+    .classList.remove("bg-btn-color", "hover:bg-btn-color");
+  document
+    .getElementById("donation-btn")
+    .classList.add("bg-btn-color", "hover:bg-btn-color");
+  hideElementById("history-section");
+  showElementById("card-section");
+});
+
+const addToDonationHistory = (title, amount) => {
+  const date = new Date();
+  const day = date.toLocaleDateString("en-US", { weekday: "short" });
+  const month = date.toLocaleDateString("en-US", { month: "short" });
+  const dayOfMonth = date.getDate();
+  const currentTime = new Date().toLocaleTimeString();
+  const newYear = new Date().getFullYear();
+  const historySection = document.getElementById("donation-section");
+  const historyData = document.createElement("div");
+  historyData.className = `border p-4 rounded-lg shadow-lg`;
+  historyData.innerHTML = `
+  <h3 class="font-bold text-lg">${title}</h3>
+  <p class="text-gray-600 text-medium text-[17px]">Donation: ${amount} TK.</p>
+  <p class="text-gray-600 opacity-60">Date: ${day}, ${month} ${dayOfMonth}, ${newYear} ${currentTime}</p>
+  `;
+  historySection.appendChild(historyData);
 };
 
 const handleDonate = (btn) => {
   const card = btn.closest(".card");
+  const cardTitle = card.querySelector(".card-title").textContent;
   const donateAmountValue = parseFloat(card.querySelector(".input").value);
   if (isNaN(donateAmountValue) || donateAmountValue < 0) {
     alert("Should be Valid");
     return;
   }
+
+  if (totalBudget.textContent < donateAmountValue) {
+    alert("You are out of your budget");
+    return;
+  }
+
   updateBudget(donateAmountValue);
   const totalCollection = parseFloat(
     card.querySelector(".total-collection").textContent
@@ -27,6 +73,9 @@ const handleDonate = (btn) => {
   card.querySelector(".total-collection").textContent = (
     totalCollection + donateAmountValue
   ).toFixed(2);
+  my_modal_1.showModal();
+
+  addToDonationHistory(cardTitle, donateAmountValue);
 
   card.querySelector(".input").value = "";
 };
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,5 +1,14 @@
-document.getElementsByTagName('html')[0].setAttribute('data-theme', 'light');
+document.getElementsByTagName("html")[0].setAttribute("data-theme", "light");
 
 const setElementById = (id, valueInput) => {
   document.getElementById(id).textContent = valueInput.toFixed(2);
+};
+
+
+const hideElementById = (id) => {
+  document.getElementById(id).classList.add('hidden')
 }
+const showElementById = (id) => {
+  document.getElementById(id).classList.remove('hidden')
+}
+
